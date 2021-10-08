31da8600f3f0fb22d2965e8f18f180e52c3a72e6
hahassan <hassan.el.hassan@sogeti.com>
2021-10-08 19:50:51 +0200

    assignment02 updated with correct naming of constants

--- Course_Part_02/src/Assignment02_Constants.java
+++ Course_Part_02/src/Assignment02_Constants.java
@@ -2,14 +2,14 @@ public class Assignment02_Constants {
 
     public static void main(String[] args) {
 
-        final int monthsImYear = 12;
-        System.out.println(monthsImYear);
+        final int MONTHS_IN_YEAR = 12;
+        System.out.println(MONTHS_IN_YEAR);
 
-        final int residentsInAmsterdam = 821_752;
-        System.out.println(residentsInAmsterdam);
+        final int RESIDENTS_IN_AMSTERDAM = 821_752;
+        System.out.println(RESIDENTS_IN_AMSTERDAM);
 
-        final float discountPercentage = 33.3f;
-        System.out.println(discountPercentage + "%");
+        final float DISCOUNT_PERCENTAGE = 33.3f;
+        System.out.println(DISCOUNT_PERCENTAGE + "%");
 
         final float PI = 3.14159265358979323f;
         System.out.println(PI);
